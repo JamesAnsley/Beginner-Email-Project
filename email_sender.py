cef0f900a853d2ffadbb7a4cbe933e4f1b0d2114
JamesAnsley <JamesAnsleyGE@gmail.com>
2020-09-17 09:50:25 -0500

    Initial commit

---
+++ email_sender.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path # similar to os.path
+
+
+html = Template(Path('index.html').read_text())
+
+email = EmailMessage()
+email["from"] = 'sender name' # replace with your info
+email["to"] = "reciever email" # replace with your info
+email["subject"] = "You won a million dollars"
+
+email.set_content(html.substitute(name='TinTin'), "html")
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('YourEmail@gmail.com', 'password') # replace with your info and do not recommit
+    smtp.send_message(email)
+    print('all good boss')
